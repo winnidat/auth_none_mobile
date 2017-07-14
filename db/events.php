5c176c8019c07668d03ca1f6e8609ce79bee8587
winidat <winkler_t@gmx.at>
2017-07-14 13:00:47 +0200

    Added user_created observer with priority

--- db/events.php
+++ db/events.php
@@ -1,8 +1,10 @@
-<?
+<?php
 
 defined('MOODLE_INTERNAL') || die();
 
 $observers = array(
     'eventname'   => '\core\event\user_created',
-    'callback'    => '\none_mobile\observer::user_created',
+    'callback'    => '\none_mobile\obs::user_created',
+    'priority'    => 200,
    );
+
